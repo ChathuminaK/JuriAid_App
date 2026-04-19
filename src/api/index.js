7398d55523f0cd55fef402b4050a3f19c9af366e
Chathumina Kaushal <161899336+ChathuminaK@users.noreply.github.com>
2026-04-20 02:23:29 +0530

    point to prod env

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-const COMPUTER_IP = '192.168.8.106'; //my router
+// const COMPUTER_IP = '192.168.8.106'; //my router
+const COMPUTER_IP = '4.194.3.165';
 //const COMPUTER_IP = '192.168.119.189'; //samidi mobile
 
 const API_URLS = {
